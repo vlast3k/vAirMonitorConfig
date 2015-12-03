90c0d58191d8ab948c104565fb282e024d182a81
vlast3k <vlast3k@gmail.com>
2015-12-03 17:23:31 +0200

    new version

--- main.js
+++ main.js
@@ -5,9 +5,22 @@ document.getElementById('co2Plus').addEventListener('click', onCo2Plus);
 document.getElementById('co2Min') .addEventListener('click', onCo2Minus);
 document.getElementById('setWifi').addEventListener('click', onSetWifi);
 document.getElementById('tsBtn').addEventListener('click', onSetTs);
-document.getElementById('btnAutoConnect').addEventListener('click', onbtnAutoConnect);
+$("#btnAutoConnect").click(onbtnAutoConnect);
 $("#btnSerialSend").click(onSerialSend);
-$("#ssid").change(onSSIDChange)
+$("#ssid").change(onSSIDChange);
+$("#sapBtn").click(onBtnSAP);
+$("#ubiBtn").click(onBtnUbi);
+$("#testCfgBtn").click(onBtnTestCfg);
+
+
+
+var stringReceived = '';
+var connectionId;
+var onOK;
+var serialString = "";
+var serialTimeout;
+var onOKString;
+
 
 function onSSIDChange() {
   if ($("#ssid").val() == "SAP-Guest") {
@@ -16,25 +29,25 @@ function onSSIDChange() {
   } else {
     $("label[for=pass]").text("Pass");
     $("#sap").addClass("hidden");
+    $("#sapPass").val("");
   }
 }
 
 
 function onSerialSend() {
-  log ($("#serial").val());
-  chrome.serial.send(connectionId, str2ab($("#serial").val() +"\n"), onSend); 
+  sendSerial($("#serial").val());
+  //chrome.serial.send(connectionId, str2ab($("#serial").val() +"\n"), onSend); 
 }
 
 
 function findDevice(idStr, onDeviceFound) {
   var devices = {};
-  var findInterval = setInterval(function() { log("#");  }, 1000);
+  var findInterval = setInterval(function() { log(" . ", true);  }, 500);
   
   function onIntDeviceFound(comPort, connId) {
     clearInterval(findInterval);
     clearTimeout(foundTimeout);
     Object.keys(devices).forEach(function(_connId) {
-      log("disconnecting: " + _connId)
      chrome.serial.disconnect(+_connId, function() {});
     });
     chrome.serial.onReceive.removeListener(onRecvFindDev);
@@ -53,33 +66,21 @@ function findDevice(idStr, onDeviceFound) {
     chrome.serial.onReceive.addListener(onRecvFindDev);
     ports.forEach(function(ppp) {
       chrome.serial.connect(ppp.path, {bitrate: 9600}, function(data) {
-        if (chrome.runtime.lastError) log (chrome.runtime.lastError.message)
-        else if (data) devices[data.connectionId] = ppp.path;
+        if (!chrome.runtime.lastError && data) devices[data.connectionId] = ppp.path;
       });
-      log ("le: " + chrome.runtime.lastError);
     });
   }
+  log ("Searching v.Air ", true);
   chrome.serial.getDevices(onGetDevices); 
 
 }
 
-
-
-
-
 function onConnect2(conn) {
-  log ("Connected to: " + JSON.stringify(conn));
-  chrome.serial.onReceive.addListener(onReceiveCallback);
+  //log ("Connected to: " + JSON.stringify(conn));
   connectionId = conn.connectionId;
+  chrome.serial.onReceive.addListener(onReceiveCallback);
 }
 
-var stringReceived = '';
-var connectionId;
-
-var onOK;
-
-var serialString = "";
-var serialTimeout;
 var onReceiveCallback = function(info) {
   var str = ab2str(info.data);
   clearTimeout(serialTimeout);
@@ -89,34 +90,73 @@ var onReceiveCallback = function(info) {
 
 function onSerialString() {
   log (serialString);
-  if (serialString.indexOf("OK") > -1) onOK && onOK();
+  if (serialString.indexOf(onOKString) > -1) {
+    onOK && onOK();
+    onOK = null;
+  }
   serialString = "";
-  
 }
 
 
- function onbtnAutoConnect() {
+function onbtnAutoConnect() {
    function onVAirFound(comPort) {
      if (!comPort) {
        document.getElementById('btnAutoConnect').className="btn btn-danger";
        document.getElementById('btnAutoConnect').value ="Not Found";
        
      } else {
-       log ("vAir found on : " + comPort); 
+       log ("\nvAir found on : " + comPort); 
        chrome.serial.connect(comPort, {bitrate: 9600}, onConnect2);
        document.getElementById('btnAutoConnect').className="btn btn-success";
        document.getElementById('btnAutoConnect').value ="Connected";
      }
      
    }
-   document.getElementById('btnAutoConnect').className="btn btn-warning";
-   document.getElementById('btnAutoConnect').value ="Searching...";
-   findDevice("vAir", onVAirFound);
- } 
+   
+   if (connectionId) {
+     chrome.serial.disconnect(connectionId, function() {
+       connectionId = null;
+       chrome.serial.onReceive.removeListener(onReceiveCallback);
+       document.getElementById('btnAutoConnect').className="btn btn-info";
+       document.getElementById('btnAutoConnect').value ="Auto Connect";
+     })
+   } else {
+     document.getElementById('btnAutoConnect').className="btn btn-warning";
+     document.getElementById('btnAutoConnect').value ="Searching...";
+     findDevice("vAir", onVAirFound);
+   }
+} 
 
  function onSetTs() {
-   log ("td:" +   document.getElementById('tsKey').value)
    chrome.serial.send(connectionId, str2ab("tskey " + document.getElementById('tsKey').value + "\n"), onSend); 
+ }
+ 
+ function sendSerial(str, _onOKString, _onOK) {
+   log("sending serial: " + str);
+   if (_onOK) {
+     onOKString = _onOKString || ">";
+     onOK = _onOK;
+   } else {
+     onOKString = ">";
+     onOK = _onOKString;
+   }
+   chrome.serial.send(connectionId, str2ab(str + "\n"), onSend); 
+ }
+ 
+ function onBtnUbi() {
+   log ("td:" +   $("#ubik").val() + "," +   $("#ubiv").val())
+   sendSerial("ubik " + $("#ubik").val(), 
+   function() {sendSerial("ubiv " + $("#ubiv").val()) } );
+ }
+ 
+ function onBtnSAP() {
+   sendSerial("proxy", "GOT IP",
+   function() {sendSerial("cfgiot \"" + $("#sapHost").val()     + "\",\""+ $("#sapDeviceId").val() + "\",\""
+                          + $("#sapMessageId").val() + "\",\""+ $("#sapVarName").val()  + "\",\"" + $("#sapToken").val()  + "\"") } )
+ }
+ 
+ function onBtnTestCfg() {
+   sendSerial("test");
  }
  
  var brg = 50; 
@@ -157,16 +197,16 @@ function onSetWifi() {
   var ssid = document.getElementById('ssid').value;
   var pass = document.getElementById('pass').value;
   var sapPass = document.getElementById('sapPass').value;
-  chrome.serial.send(connectionId, str2ab("wifi \"" + ssid + "\",\""+ pass + "\"" + (sapPass?",\"" + sapPass + "\"":":") + "\n"), onSend);
+  chrome.serial.send(connectionId, str2ab("wifi \"" + ssid + "\",\""+ pass + "\"" + (sapPass?",\"" + sapPass + "\"":"") + "\n"), onSend);
 }
 
 function onSend(data) {
 }
 
- function log(msg) {
+ function log(msg, skipNL) {
    var buffer = document.querySelector('#buffer');
-   buffer.value += msg +"\n";
+   buffer.value += msg + (skipNL?"":"\n");
    buffer.scrollTop = buffer.scrollHeight;
  }
 
-
+onbtnAutoConnect();
--- utils.js
+++ utils.js
@@ -2,12 +2,12 @@
 var ab2str = function(buf) {
   var bufView = new Uint8Array(buf);
   var encodedString = String.fromCharCode.apply(null, bufView);
-  return decodeURIComponent(escape(encodedString));
+  return encodedString;
 };
 
 /* Converts a string to UTF-8 encoding in a Uint8Array; returns the array buffer. */
 var str2ab = function(str) {
-  var encodedString = unescape(encodeURIComponent(str));
+  var encodedString = str;
   var bytes = new Uint8Array(encodedString.length);
   for (var i = 0; i < encodedString.length; ++i) {
     bytes[i] = encodedString.charCodeAt(i);
