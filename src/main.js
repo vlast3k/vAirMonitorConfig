ed003ab0f5e14a98ab832c0a4f68a1c8e4c1d881
Vladi Vladi <vlast3k@gmail.com>
2016-12-27 17:10:56 +0200

    Now takes local ip and searches subnetwork

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ String.prototype.format = function() {
 var sockets = [];
 
 function tryWSS(i, onFound) {
-  var ws = new WebSocket("ws://192.168.1." + i + ":81");
+  var ws = new WebSocket("ws://" + i +  ":81");
   sockets.push(ws);
   ws.onopen = function(evt) {
     log ("Found WebSocketServer on:" + i);
@@ -42,10 +42,25 @@ function tryWSS(i, onFound) {
   // ws.onmessage = function(evt) { };
   // ws.onerror = function(evt) { console.log("onerror: " + i);};
 }
-function searchServer(onFound) {
+
+function browseOneNet(ip, onFound) {
+  var base = ip.substring(0, ip.lastIndexOf('.') + 1);
+  console.log("will search base: " + base);
   for (var i=1; i < 255; i++) {
-    tryWSS(i, onFound);
+    tryWSS(base + i, onFound);
   }
+}
+
+function browseNetworks(ips, onFound) {
+  ips.forEach(function(ip) {
+    browseOneNet(ip, onFound);
+  });
+}
+
+function searchServer(onFound) {
+  getIPs(function(ips) {
+    browseNetworks(ips, onFound);
+  });
 }
 var wsclient;
 function initWebSocketClient(ws, onFound) {
@@ -216,6 +231,52 @@ function init() {
   if (chrome.serial) AutoConnect.onbtnAutoConnect();
 
   $('#mainTabs a[href="#setupWifi"]').tab("show");
+}
+
+function getIPs(onSuccess) {
+  var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+  if (!RTCPeerConnection) return;
+  var rtc = new RTCPeerConnection({iceServers:[]});
+  if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+      rtc.createDataChannel('', {reliable:false});
+  };
+
+  rtc.onicecandidate = function (evt) {
+      // convert the candidate to SDP so we can run it through our general parser
+      // see https://twitter.com/lancestout/status/525796175425720320 for details
+      if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
+  };
+  rtc.createOffer(function (offerDesc) {
+      grepSDP(offerDesc.sdp);
+      rtc.setLocalDescription(offerDesc);
+  }, function (e) { console.warn("offer failed", e); });
+
+
+  var addrs = Object.create(null);
+  addrs["0.0.0.0"] = false;
+  function updateDisplay(newAddr) {
+      if (newAddr in addrs) return;
+      else addrs[newAddr] = true;
+      var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
+      onSuccess(displayAddrs);
+      //document.getElementById('list').textContent = displayAddrs.join(" or perhaps ") || "n/a";
+  }
+
+  function grepSDP(sdp) {
+      var hosts = [];
+      sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+          if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+              var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                  addr = parts[4],
+                  type = parts[7];
+              if (type === 'host') updateDisplay(addr);
+          } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+              var parts = line.split(' '),
+                  addr = parts[2];
+              updateDisplay(addr);
+          }
+      });
+  }
 }
 
 
